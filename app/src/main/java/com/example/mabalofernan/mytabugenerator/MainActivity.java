081eadf50ed23d263dc18e50f4a0910689aa6ba8
Miriam Abalo Fernandez <mabalofernan@CSCESPAI422587.emea.globalcsc.net>
2019-01-30 11:33:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mabalofernan/mytabugenerator/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mabalofernan.mytabugenerator;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button manageThemesButton;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        manageThemesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent intent = new Intent(MainActivity.this, ManageThemesActivity.class);
+              startActivity(intent);
+            }
+        });
+
+    }
+
+    public void init(){
+        manageThemesButton = findViewById(R.id.manage);
+        context = getApplicationContext();
+    }
+
+}
